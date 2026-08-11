8c6b82bc2e05529096c0f81e1b60ea5dc264accf
JeanPale <dudko.vania@yandex.ru>
2026-08-11 23:56:54 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,35 @@
+from uuid import uuid4
+
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['http://localhost:3000'],
+    allow_methods=["*"],
+)
+
+class TaskSchema(BaseModel):
+    id: str
+    title: str
+    completed: bool
+
+class TaskCreateSchema(BaseModel):
+    title: str
+
+tasks: list[TaskSchema] = []
+
+@app.get('/tasks')
+def read_tasks():
+    return tasks
+
+@app.post('/tasks')
+def create_task(payload: TaskCreateSchema) -> TaskSchema:
+    new_task = TaskSchema(id=str(uuid4()),
+                           title=payload.title,
+                           completed=False)
+    tasks.append(new_task)
+    return new_task
